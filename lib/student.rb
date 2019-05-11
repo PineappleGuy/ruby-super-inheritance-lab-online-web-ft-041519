a1589fa5970ed0a4d4eecd73e337494755a237fa
Garth Lilly <garff247@gmail.com>
2019-05-11 00:18:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    "Pick me!\n"
+    puts "Pick me!"
   end
 
 end
